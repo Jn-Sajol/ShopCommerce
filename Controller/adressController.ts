728e9d35573c929305b7a47a307ff8e90e28272d
Jn_Sajol <julkar10121@gmail.com>
2025-01-21 22:47:15 +0600

    updating relational databse

---
+++ Controller/adressController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+const createAdress = async (req:Request , res:Response) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+const getallAdress = async (req:Request , res:Response) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+const updateAdress = async (req:Request , res:Response) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
